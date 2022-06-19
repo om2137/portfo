7b5d9c119dbed7500953f5ae70a6df093b5e5c14
om2137 <omraut961@gmail.com>
2022-06-19 23:46:18 +0530

    content after intro

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,16 +11,25 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col justify-top pt-10 px-20 ">
-        <div>
+        <div className=''>
+          <div className='flex items-left '>
           <div className='flex items-center justify-center w-100 pb-4 '>
-          <img src="https://avatars.githubusercontent.com/u/33827410?s=400&u=d7fa33a6aba54a8748942939d48217d9ba0fcf84&v=4"  className="flex flex-col rounded-full ml-4" width="250" />
+              <img src="https://avatars.githubusercontent.com/u/33827410?s=400&u=d7fa33a6aba54a8748942939d48217d9ba0fcf84&v=4"  className="flex flex-col rounded-full ml-4" width="250" />
               <div className='flex flex-col justify-start mr-4'>
                 <span className='text-left text-6xl ml-6 font-bold text-green-500'>Om Raut</span>
                 <span className='text-sm text-white text-left text-l pl-6 pt-2'>github, twitter, linkedin, hashnode, gmail</span> {/* all socials */}
                 <span className='text-left pl-6 pt-2 text-gray-400'>paragraph about introduction</span> 
               </div>
           </div>
-              
+
+          </div>
+          
+          <div className='flex flex-col items-left text-4xl justify center w-100 pb-4 pt-6 '>
+            <span className='text-white font-bold pl-10 pb-5'>Projects</span>
+            <span className='text-white font-bold pl-10 pb-5'>Work Experience</span>
+            <span className='text-white font-bold pl-10 pb-5'>Skill</span>
+            <span className='text-white font-bold pl-10 pb-5'>NonTechnical</span>
+          </div>    
         </div>
         
         
