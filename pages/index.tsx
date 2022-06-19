b88fa274fe2bc55addc62f077f25d564a1a533fc
om2137 <omraut961@gmail.com>
2022-06-19 23:32:16 +0530

    top

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,24 +4,23 @@ import Image from 'next/image'
 
 const Home: NextPage = () => {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2 dark:bg-slate-900 ">
+    <div className="flex  min-h-screen flex-col items-center justify-center py-2 dark:bg-slate-900 ">
       <Head>
         <title>Om Raut</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-top pt-10 px-20 text-center  ">
+      <main className="flex w-full flex-1 flex-col justify-top pt-10 px-20 ">
         <div>
-          <div className='flex items-center justify-center w-100 pb-4'>
-              <img src="https://avatars.githubusercontent.com/u/33827410?s=400&u=d7fa33a6aba54a8748942939d48217d9ba0fcf84&v=4"  className="flex rounded-full " width="300" height="170" />
-              <div className='flex flex-col justify-start '>
-                <h1 className='flex flex-col text-left text-9xl font-bold text-green-500 pl-8 text-shadow-yellow'>Om Raut</h1>
-                
-                {/*  */}
-                <span className='text-sm text-gray-400 text-left text-2xl pl-10 pt-1'>@Elevate.ai</span>
-                <span className='text-left pl-10 pt-2 text-white'>paragraph about introduction</span>
+          <div className='flex items-center justify-center w-100 pb-4 '>
+          <img src="https://avatars.githubusercontent.com/u/33827410?s=400&u=d7fa33a6aba54a8748942939d48217d9ba0fcf84&v=4"  className="flex flex-col rounded-full ml-4" width="250" />
+              <div className='flex flex-col justify-start mr-4'>
+                <span className='text-left text-6xl ml-6 font-bold text-green-500'>Om Raut</span>
+                <span className='text-sm text-white text-left text-l pl-6 pt-2'>github, twitter, linkedin, hashnode, gmail</span> {/* all socials */}
+                <span className='text-left pl-6 pt-2 text-gray-400'>paragraph about introduction</span> 
               </div>
-            </div>
+          </div>
+              
         </div>
         
         
