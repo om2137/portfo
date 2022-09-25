205abe2421e02723a3d544674b4383d7fb2ca87a
om2137 <omraut961@gmail.com>
2022-09-25 18:58:54 +0530

    footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import { useState } from 'react'
+import { styled, Theme } from '@mui/material';
+import { makeStyles } from '@material-ui/core/styles'
+
+
+
+
+export default function Footer() {
+  
+
+  return (
+    <>
+    <div className="flex w-full flex-1 flex-col ">
+          <div className="flex justify-center items-center rounded-b-lg border-b border-green-500  py-6 xl:py-8 bg-slate-800 xl:px-20">
+
+            
+            <span className="text-center text-4xl font-semibold text-white">Thank you for visiting my website!</span>
+            
+            
+            
+          </div>
+      </div>
+    </>
+  )
+}
+
