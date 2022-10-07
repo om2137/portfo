8861be6d947695d2c568fcd58acd12f9b08641a6
om2137 <omraut961@gmail.com>
2022-10-07 23:24:10 +0530

    text sizing; spacing and padding in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -14,8 +14,8 @@ export default function Footer() {
   return (
     <>
     <div className="flex w-full flex-1 flex-col ">
-          <div className="flex justify-center items-center rounded-b-lg  py-6 xl:py-8 bg-slate-800 xl:px-20">
-            <span className="text-center text-4xl font-semibold text-white">Thank you for visiting my website!</span>
+          <div className="flex justify-center items-center rounded-b-lg  pt-6 pb-8 xl:py-8 bg-slate-800 xl:px-20">
+            <span className="text-center sm:text-4xl text-lg font-semibold text-white font-bluureg">Thank you fo visiting my website!</span>
           </div>
       </div>
     </>
